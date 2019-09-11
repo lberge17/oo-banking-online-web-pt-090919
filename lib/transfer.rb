009ae5baebb2be3c5ea346c0eb528396c6b04d38
Laura Berge <lauraeberge@gmail.com>
2019-09-11 21:40:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,10 @@
 class Transfer
-  attr_accessor :sender, :receiver
+  attr_accessor :sender, :receiver, :status
   
   def initialize(sender, receiver, balance)
     @sender = sender
     @receiver = receiver
+    @status = "pending"
   end
   
 end
