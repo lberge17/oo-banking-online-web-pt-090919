2d86fc86d97e1b8aa14c2faadfecf6776d6bce99
Laura Berge <lauraeberge@gmail.com>
2019-09-11 21:58:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
     if @status = "pending"
       if valid? && sender.balance >= @amount
         @status = "complete"
-        @sender.balance += @amount
-        @receiver.balance -= @amount
+        @sender.balance -= @amount
+        @receiver.balance += @amount
       else
         @status = "rejected"
         p "Transaction rejected. Please check your account balance."
@@ -31,8 +31,8 @@ class Transfer
   
   def reverse_transfer
     if @status = "complete"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
+      @sender.balance += @amount
+      @receiver.balance -= @amount
       @status = "reversed"
     end
   end
