fa8a5130881b82a9c84b3030ac5c68a0bb7e1de4
Laura Berge <lauraeberge@gmail.com>
2019-09-11 22:01:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status = "pending"
+    if @status == "pending"
       if valid? && sender.balance >= @amount
         @status = "complete"
         @sender.balance -= @amount
@@ -29,7 +29,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
